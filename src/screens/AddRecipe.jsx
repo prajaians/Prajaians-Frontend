2ffffb5a2966b8bffada4b30c8a1a064ef791510
Alex Lison <alexlison910@gmail.com>
2026-07-11 12:40:52 +0530

    recheck Addrecipe

--- src/screens/AddRecipe.jsx
+++ src/screens/AddRecipe.jsx
@@ -573,7 +573,7 @@ const AddRecipe = () => {
                           {imageUrl ? (
                             <div className="w-8 h-8 rounded-lg overflow-hidden border border-[#c9a962]/20 flex-shrink-0">
                               <img 
-                                src={ing.imageUrl}
+                                src={imageUrl}
                                 alt={ing.ingredientName} 
                                 className="w-full h-full object-cover"
                                 onError={(e) => {
